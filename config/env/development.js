db11881e1bbb91c152221a1e0286f550a2180ad4
Mashcode <marc@marcshifflett.com>
2014-03-02 14:24:12 -0500

    initial commit

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
     db: 'mongodb://localhost/mean-dev',
     app: {
-        name: 'MEAN - A Modern Stack - Development'
+        name: 'GiantSteps - Transit Counter &amp; Tracker - Development'
     },
     facebook: {
         clientID: 'APP_ID',
